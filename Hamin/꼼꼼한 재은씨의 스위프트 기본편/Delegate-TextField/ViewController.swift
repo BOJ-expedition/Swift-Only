d6873e7ab3d6d39240461a7c09ad2f91cb313cc9
naldal <cec7378@gmail.com>
2021-02-12 20:14:14 +0900

    firstResponder ex

--- Hamin/꼼꼼한 재은씨의 스위프트 기본편/Delegate-TextField/ViewController.swift
+++ Hamin/꼼꼼한 재은씨의 스위프트 기본편/Delegate-TextField/ViewController.swift
@@ -16,9 +16,32 @@ class ViewController: UIViewController {
         self.tf.keyboardAppearance = UIKeyboardAppearance.dark // 키보드 스타일 어둡게
         self.tf.returnKeyType = UIReturnKeyType.join // 리턴키 타입은 "Join"
         self.tf.enablesReturnKeyAutomatically = true // 리턴키 자동활성화 "ON"
+        
+        // 스타일 설정
+        
+        // 테두리 스타일 - 직선
+        self.tf.borderStyle = UITextField.BorderStyle.line
+        // 배경 색상
+        self.tf.backgroundColor = UIColor(white: 0.87, alpha: 1.0)
+        // 수직 방향으로 텍스트가 가운데 수정되도록
+        self.tf.contentVerticalAlignment = .center
+        // 수평 방향으로 텍스트가 가운데 정렬되도록
+        self.tf.contentHorizontalAlignment = .center
+        // 테두리 색상을 회색으로
+        self.tf.layer.borderColor = UIColor.darkGray.cgColor
+        // 테두리 두께 설정 (단위: Pt)
+        self.tf.layer.borderWidth = 2.0
+        
+        
+        /// 텍스트 필드를 최초 응답자로 지정
+        self.tf.becomeFirstResponder()
+        
     }
 
     @IBOutlet var tf: UITextField!
     
+    @IBAction func confirm(_ sender: Any) {
+        self.tf.resignFirstResponder()
+    }
 }
 
